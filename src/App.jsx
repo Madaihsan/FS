45d99eeabbc0e08928aca0e8941231e7ecae17d3
Mada-ihsan <2300016158@webmail.uad.ac.id>
2025-06-27 02:22:37 +0700

    style: memisahkan pengerjaan Auth dan create Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Guest from './pages/auth/Guest'
+import Guest from './pages/auth/guest'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/register'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import MainLayout from './layouts/MainLayout'
 
 export default function App() {
   return (
@@ -14,9 +11,6 @@ export default function App() {
         <Route path="/" element={<Guest />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/Navbar" element={<Navbar />} />
-        <Route path="/Sidebar" element={<Sidebar />} />
-        <Route path="/MainLayout" element={<MainLayout />} />
       </Routes>
     </Router>
   )
