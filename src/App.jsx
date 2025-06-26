a995a34d36058aad18f96dc0558ce2dfac44d7f6
Mada-ihsan <2300016158@webmail.uad.ac.id>
2025-06-27 02:32:50 +0700

    feat: Membuat Homepage dengan Layout dashboar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Guest from './pages/auth/guest'
 import Login from './pages/auth/Login'
 import Register from './pages/auth/register'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import MainLayout from './layouts/MainLayout'
 
 export default function App() {
   return (
@@ -11,6 +13,9 @@ export default function App() {
         <Route path="/" element={<Guest />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/Navbar" element={<Navbar />} />
+        <Route path="/Sidebar" element={<Sidebar />} />
+        <Route path="/MainLayout" element={<MainLayout />} />
       </Routes>
     </Router>
   )
